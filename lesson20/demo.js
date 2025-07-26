10c1f7e9fe2044b7d6e8b372d02506a050eb72ba
Udhay178 <malkeetsidhu974@gmail.com>
2025-07-26 21:23:35 +0530

    Functions in JavaScript

---
+++ lesson20/demo.js
@@ -0,0 +1,21 @@
+function Codingal() {
+    console.log("JavaScript")
+    console.log("Loading....")
+}
+
+Codingal()
+Codingal()
+
+function sum(a, b, c) {
+    var result = a + b + c
+    return result
+}
+// console.log(result)
+
+var a = sum(3,9,4)
+console.log(a)
+
+function multiply(a,b,c,d,e){
+    return a*b*c*d*e
+}
+console.log(multiply(2,3,4,5,6))
