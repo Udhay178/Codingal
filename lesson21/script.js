1a04eb67e54d83820b1922be86ca8b121a5876c6
Udhay178 <malkeetsidhu974@gmail.com>
2025-08-02 21:36:12 +0530

    Loop

---
+++ lesson21/script.js
@@ -0,0 +1,19 @@
+function ToSaySorry(a, b) {
+    return a + b
+}
+
+var a = ToSaySorry(3, 2)
+console.log(a)
+
+var b = { age: 99, hwaa: function (s, r, y) { return s * r * y } }
+
+var c = b.hwaa()
+
+console.log(c)
+
+var d = 'Sorry, forgive me'
+
+var i
+for (i = 0; i < 20; i++) {
+    console.log(c)
+}
