4a4d91267acacdf5d088b583cb639c3d152c59e0
Udhay178 <malkeetsidhu974@gmail.com>
2025-08-08 21:24:34 +0530

    Objects and methods in JS

---
+++ lesson22/script.js
@@ -0,0 +1,15 @@
+var walid = {
+    name: 'Udhaypartap', age: '13', city: 'Bathinda', country: 'India',
+    printDetails: function () {
+        console.log('huahuahuahahahaaaahahahaa')
+        return 106
+    }
+}
+
+console.log(walid.name)
+console.log(walid['city'])
+
+console.log(walid.printDetails())
+
+console.log(Math.ceil(9))
+console.log(Math.random())
