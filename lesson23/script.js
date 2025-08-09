574c1da46622cdd2fac41c70a009669863d8b4be
Udhay178 <malkeetsidhu974@gmail.com>
2025-08-09 21:31:06 +0530

    array  and call stack in js

---
+++ lesson23/script.js
@@ -0,0 +1,17 @@
+var names = ['chotadon', 'gotyaa', 'lappu', 'chotya', 106, 666, true]
+var happyrakhi = ['riya', 'chotibhachihokya', 'tigershroff']
+
+console.log(names[0])
+console.log(happyrakhi[1])
+
+function sum(a,b,c){
+    return a+b+c
+}
+
+function average(a,b,c){
+    return sum(a,b,c)/3
+}
+
+var result = average(1,2,3)
+
+console.log(result)
