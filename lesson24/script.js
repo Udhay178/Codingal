b9e3c5fe7a5988f9b86999d60559779d32e9c43c
Udhay178 <malkeetsidhu974@gmail.com>
2025-08-15 21:28:54 +0530

    HTML DOM

---
+++ lesson24/script.js
@@ -0,0 +1,11 @@
+var e1 = document.getElementById("Bangladesh")
+var e1 = document.getElementById("India")
+
+e1.innerHTML = 'Happy Indepedence Day to India and Bangladesh'
+
+var e2s = document.getElementsByClassName('Both are friends')
+
+e2s[0].style.width = '105px'
+e2s[0].style.backgroundColor = 'Orange'
+e2s[1].style.width = '130px'
+e2s[1].style.backgroundColor = 'Green'
